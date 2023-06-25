2881b5d2ed40a35aeed3d39645bcfc7c491b2d5f
JungKangWoo <todendvv91@gmail.com>
2023-06-25 19:39:24 +0900

    mainLastUp

--- js/web_scroll_toggle.js
+++ js/web_scroll_toggle.js
@@ -1,21 +1,27 @@
+const getWindowWidth = () => {
+  return (
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth
+  );
+};
+
 const headerContainer = document.getElementById("header_Container");
 const headerImageContainer = document.getElementById("header_ImageContainer");
 
-const fadeOut = () => {
-  headerImageContainer.style.top = "-300px";
-};
-
-const fadeIn = () => {
-  headerImageContainer.style.top = "0";
-};
-
 const headerObserver = new IntersectionObserver(
   (entries) => {
+    const windowWidth = getWindowWidth(); // 창 너비를 동적으로 확인
+
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        fadeOut();
+      if (windowWidth >= 1024) {
+        if (entry.isIntersecting) {
+          headerImageContainer.style.top = "-300px"; // 상단에 고정
+        } else {
+          headerImageContainer.style.top = "0"; // 상단에 고정
+        }
       } else {
-        fadeIn();
+        headerImageContainer.style.top = "0"; // 상단에 고정
       }
     });
   },
