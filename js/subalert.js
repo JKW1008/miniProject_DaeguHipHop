4a3e23129cf062289c0ac3237a8615213e8473fb
JungKangWoo <todendvv91@gmail.com>
2023-06-28 10:20:12 +0900

    lastUpdate

---
+++ js/subalert.js
@@ -0,0 +1,11 @@
+const elements = document.getElementsByClassName("alertbtn");
+
+function showAlert() {
+  alert(
+    "2023년 대구힙합페스티벌 행사는 종료되었습니다. 내년에 다시 찾아 뵙겠습니다."
+  );
+}
+
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", showAlert);
+}
