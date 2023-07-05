8406f69bbbcaa949cac567ef4f46346bdff5b08d
JungKangWoo <todendvv91@gmail.com>
2023-07-05 14:34:15 +0900

    lastUpdate

--- js/accordion.js
+++ js/accordion.js
@@ -1,25 +1,9 @@
-function toggleAccordion() {
-  const isActive = this.classList.contains("open");
-
-  // 모든 아코디언 메뉴 닫기
-  accordionItems.forEach((item) => {
-    item.classList.remove("open");
-    item.querySelector("ul").style.display = "none";
-  });
-
-  // 클릭한 아코디언 메뉴 열기 또는 닫기
-  if (!isActive) {
-    this.classList.add("open");
-    this.querySelector("ul").style.display = "block";
+var menuItems = document.querySelectorAll(".menu li");
+menuItems.forEach(function (item) {
+  var submenu = item.querySelector("ul");
+  if (submenu) {
+    item.addEventListener("click", function () {
+      item.classList.toggle("open");
+    });
   }
-}
-
-const accordionItems = document.querySelectorAll(".sidebar li.open");
-
-accordionItems.forEach((item) => {
-  // 초기에 모든 아코디언 메뉴 닫기
-  item.classList.remove("open");
-  item.querySelector("ul").style.display = "none";
-
-  item.addEventListener("click", toggleAccordion);
 });
